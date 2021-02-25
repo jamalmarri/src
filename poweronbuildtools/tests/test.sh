569674764acd2c8cf25699d5a8e55625a7e4f494
gideonwolfe <wolfeg2@wwu.edu>
2021-02-24 17:01:22 -0800

    some more statements

--- poweronbuildtools/tests/test.sh
+++ poweronbuildtools/tests/test.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-echo "[poweron] Beginning test script"
+echo -e "\033[38;5;6m[INFO]\033[0m Beginning test script"
 
-# query dmesg. Do we see any 
+FAILED_TESTS=0
+PASSED_TESTS=0
 
-
-
+# query dmesg. Do we see any output from the attach/match functions?
+if sudo dmesg | grep -q "RTC8523 TEST"; then
+  echo -e "\033[38;5;2m[SUCCESS]\033[0m dmesg output detected"
+else
+  echo -e "\033[31;1;4m[FAILURE]\033[0m no dmesg output detected"
+fi
 
 # Test to see if RTC device is mounted
 # i2cscan iic0 should return 0x68
 
-
-echo "[poweron] Ending test script"
+echo -e "\033[38;5;6m[INFO]\033[0m Ending test script"
