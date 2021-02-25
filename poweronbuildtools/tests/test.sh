1f744cbaa7a03dcce65c7aab7fac580dc444039c
gideonwolfe <wolfeg2@wwu.edu>
2021-02-24 16:44:49 -0800

    skeleton test

---
+++ poweronbuildtools/tests/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "[poweron] Beginning test script"
+
+# query dmesg. Do we see any 
+
+
+
+
+# Test to see if RTC device is mounted
+# i2cscan iic0 should return 0x68
+
+
+echo "[poweron] Ending test script"
