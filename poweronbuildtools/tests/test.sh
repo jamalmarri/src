894e774df589b93e774c224250094fe41a6121dc
gideonwolfe <wolfeg2@wwu.edu>
2021-02-25 18:32:07 -0800

    Some skeleton commands

--- poweronbuildtools/tests/test.sh
+++ poweronbuildtools/tests/test.sh
@@ -6,21 +6,39 @@ TOTAL_TESTS=3
 FAILED_TESTS=0
 PASSED_TESTS=0
 
+# Test1
 # query dmesg. Do we see any output from the attach/match functions?
 if dmesg | grep -q "RTC8523 TEST"; then
   echo -e "\033[38;5;2m[SUCCESS]\033[0m dmesg output detected"
   ((PASSED_TESTS=PASSED_TESTS+1))
 else
-  echo -e "\033[31;1;4m[FAILURE]\033[0m no dmesg output detected"
+  echo -e "\033[31;1;4m[FAILURE]\033[0m No dmesg output detected"
   ((FAILED_TESTS=FAILED_TESTS+1))
 fi
 
+# Test2
 # Test to see if RTC device is mounted
 # i2cscan iic0 should return 0x68
 
-echo -e "\033[38;5;6m[INFO]\033[0m Ending test script"
 
+# Test3
+# Fetch time/date from RTC
+
+
+# Test4
+# Write data to RTC
+
+
+# Test5
+# Read same data back from RTC
+
+
+
+# Test6
 # Print the results
+echo -e "\033[38;5;6m[INFO]\033[0m Ending test script"
+echo '------------------'
 echo -e "\033[38;5;2m[INFO] Tests Passed:\033[0m ${PASSED_TESTS}"
 echo -e "\033[38;5;1m[INFO] Tests Failed:\033[0m ${FAILED_TESTS}"
 echo -e "\033[38;5;6m[INFO] Results:\033[0m ${PASSED_TESTS}/${TOTAL_TESTS}"
+echo '------------------'
